ed46b5a6abb34059f13d8fff1a6037a9500134ab
retrokyo <froningr@gmail.com>
2021-02-05 16:48:05 +0900

    initial outline and variables

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,5 +1,21 @@
 package studentdatabaseapp;
 
 public class Student {
-    
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentId;
+    private String courses;
+    private int tuitionBalance;
+    private int courseCost = 600;
+
+    // Constructor: prompt user to enter students name and year
+
+    // Generate an ID
+
+    // Enroll in courses
+
+    // View balance and pay tuition
+
+    // Show student status
 }
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -2,6 +2,8 @@ package studentdatabaseapp;
 
 public class StudentDatabaseApp {
     public static void main(String[] args) throws Exception {
-        
+        // Ask how many users will need to be added
+
+        // Create n number of new students
     }
 }
