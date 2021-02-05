a443a6be1b528ddaba18987ba3a7c69333ecc4ab
retrokyo <froningr@gmail.com>
2021-02-05 18:07:33 +0900

    interface implemntation

--- src/bankaccountapp/Account.java
+++ src/bankaccountapp/Account.java
@@ -1,5 +1,9 @@
 package bankaccountapp;
 
-public class Account {
-    
+public abstract class Account implements IBaseRate {
+    // List common properties for savings and checking accounts
+
+    // Constructor set base properties and initialize the account
+
+    // List common methods
 }
